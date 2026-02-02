2bbf0b0d2622753745992219205110134ec067c6
venkatasaiutm <venkatasaiutm@stgit.com>
2026-02-02 16:16:32 +0530

    Initial commit: Spring Boot Azure AD Backend

---
+++ src/main/java/com/springboot/azure/Controller/AzureController.java
@@ -0,0 +1,22 @@
+package com.springboot.azure.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class AzureController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("message", "The backend is running!");
+        return response;
+    }
+
+
+}
